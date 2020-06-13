f712a09b8cb6e64d557878cf5b32b75892c08206
mbatista4 <manuelbatista04@gmail.com>
2020-06-13 01:36:59 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+//Configuring Server views
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+//Connecting to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+const indexRoute = require('./routes/index');
+app.use('/', indexRoute);
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Connected to Port: " + 3000)
+});
